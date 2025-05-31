cac5392198c4ae3c1ef3c73711a49603f29ce63d
MatchaBreak <stefanmb0@gmail.com>
2025-05-31 10:14:16 +1000

    new dispatcher working, needs problem setting

--- src/my_sim_pkg/src/dispatcher.py
+++ src/my_sim_pkg/src/dispatcher.py
@@ -1,3 +1,4 @@
+
 import rospy
 import random
 from copy import deepcopy
@@ -13,148 +14,148 @@ from my_sim_pkg.srv import (
     SignalOrderReadyRequest
 )
 
-min_pizzas_per_order = 1
-max_pizzas_per_order = 10
-robot_load_capacity = 15
-number_of_stops_per_trip = 4
+class Dispatcher:
+    def __init__(self):
+        rospy.init_node("dispatcher")
+        rospy.wait_for_service("/get_house_locations")
+        rospy.wait_for_service("/listen_to_queue")
+        rospy.wait_for_service("/signal_order_ready")
+        rospy.wait_for_service("/listen_for_order_taken")
 
-def create_distance_matrix(locations):
-    def _manhattan_distance(p1, p2):
-        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-    if not locations:
-        return []
-    depot_loc = locations[0]
-    customer_locs = locations[1:]
-    n_customers = len(customer_locs)
-    matrix_size = n_customers + 2
-    dist_matrix = [[0] * matrix_size for _ in range(matrix_size)]
-    for j in range(n_customers):
-        customer_matrix_idx = j + 1
-        dist_matrix[0][customer_matrix_idx] = _manhattan_distance(depot_loc, customer_locs[j])
-    for i in range(n_customers):
-        current_customer_loc = customer_locs[i]
-        matrix_row_idx = i + 1
+        self.get_houses = rospy.ServiceProxy("/get_house_locations", GetHouseLocations)
+        self.listen_queue = rospy.ServiceProxy("/listen_to_queue", ListenToQueue)
+        self.signal_ready = rospy.ServiceProxy("/signal_order_ready", SignalOrderReady)
+        self.listen_taken = rospy.ServiceProxy("/listen_for_order_taken", ListenForOrderTaken)
+
+        self.min_pizzas = 1
+        self.max_pizzas = 10
+        self.load_capacity = 15
+        self.stops_per_trip = 4
+        self.rate = rospy.Rate(1)
+        self.route_queue = []
+
+        self.houses = []
+        self.building_positions = []
+        self.demand_each_house = {}
+
+        self.setup()
+
+    def create_distance_matrix(self, locations):
+        def _manhattan_distance(p1, p2):
+            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+        if not locations:
+            return []
+        depot_loc = locations[0]
+        customer_locs = locations[1:]
+        n_customers = len(customer_locs)
+        matrix_size = n_customers + 2
+        dist_matrix = [[0] * matrix_size for _ in range(matrix_size)]
         for j in range(n_customers):
-            matrix_col_idx = j + 1
-            dist_matrix[matrix_row_idx][matrix_col_idx] = _manhattan_distance(current_customer_loc, customer_locs[j])
-        sink_matrix_idx = matrix_size - 1
-        dist_matrix[matrix_row_idx][sink_matrix_idx] = _manhattan_distance(current_customer_loc, depot_loc)
-    return dist_matrix
+            customer_matrix_idx = j + 1
+            dist_matrix[0][customer_matrix_idx] = _manhattan_distance(depot_loc, customer_locs[j])
+        for i in range(n_customers):
+            matrix_row_idx = i + 1
+            for j in range(n_customers):
+                matrix_col_idx = j + 1
+                dist_matrix[matrix_row_idx][matrix_col_idx] = _manhattan_distance(customer_locs[i], customer_locs[j])
+            dist_matrix[matrix_row_idx][-1] = _manhattan_distance(customer_locs[i], depot_loc)
+        return dist_matrix
 
-def dispatcher():
-    rospy.init_node("dispatcher")
-    rospy.wait_for_service("/get_house_locations")
-    rospy.wait_for_service("/listen_to_queue")
-    rospy.wait_for_service("/signal_order_ready")
-    rospy.wait_for_service("/listen_for_order_taken")
-    get_houses = rospy.ServiceProxy("/get_house_locations", GetHouseLocations)
-    listen_queue = rospy.ServiceProxy("/listen_to_queue", ListenToQueue)
-    signal_ready = rospy.ServiceProxy("/signal_order_ready", SignalOrderReady)
-    listen_taken = rospy.ServiceProxy("/listen_for_order_taken", ListenForOrderTaken)
-
-    try:
-        house_response = get_houses()
-    except rospy.ServiceException:
-        rospy.logerr("dispatcher: failed to call /get_house_locations")
-        return
-
-    houses = []
-    data = house_response.locations.data
-    for i in range(0, len(data) - 1, 2):
-        houses.append((data[i], data[i + 1]))
-
-    if not houses:
-        rospy.logwarn("dispatcher: no house locations found")
-        return
-
-    rospy.loginfo("dispatcher: initialised with %d house(s)", len(houses))
-    for idx, house in enumerate(houses, 1):
-        rospy.loginfo("dispatcher: House %d is located at (%d, %d)", idx, house[0], house[1])
-
-    rate = rospy.Rate(1)
-    demand_each_house = {i: random.randrange(min_pizzas_per_order, max_pizzas_per_order) for i in range(len(houses))}
-    building_positions = [(8, 8)] + [deepcopy(h) for h in houses]
-    distance_matrix = create_distance_matrix(building_positions)
-
-    while not rospy.is_shutdown():
+    def setup(self):
         try:
-            queue_response = listen_queue()
+            response = self.get_houses()
+            data = response.locations.data
+            self.houses = [(data[i], data[i + 1]) for i in range(0, len(data) - 1, 2)]
         except rospy.ServiceException:
-            rospy.logerr("dispatcher: failed to call /listen_to_queue")
-            rate.sleep()
-            continue
+            rospy.logerr("dispatcher: failed to get house locations")
+            return
 
-        bot_id = queue_response.nextInQueue
-        if bot_id < 0:
-            rospy.loginfo("dispatcher: No robots in queue.")
-            rospy.sleep(2)
-            continue
+        rospy.loginfo(f"dispatcher: initialised with {len(self.houses)} house(s)")
+        for idx, house in enumerate(self.houses, 1):
+            rospy.loginfo(f"dispatcher: House {idx} is located at {house}")
 
-        rospy.loginfo("dispatcher: Robot %d is next in queue", bot_id)
-        house = random.choice(houses)
-        rospy.loginfo("dispatcher: assigning delivery to house at (%d, %d)", *house)
+        self.demand_each_house = {i: random.randint(self.min_pizzas, self.max_pizzas) for i in range(len(self.houses))}
+        self.building_positions = [(8, 8)] + [deepcopy(h) for h in self.houses]
+        self.distance_matrix = self.create_distance_matrix(self.building_positions)
 
-        A = array(distance_matrix, dtype=[("cost", int)])
+    def generate_routes(self):
+        A = array(self.distance_matrix, dtype=[("cost", int)])
         G = from_numpy_array(A, create_using=DiGraph())
-        set_node_attributes(G, values=demand_each_house, name="demand")
-        G = relabel_nodes(G, {0: "Source", len(building_positions): "Sink"})
-
-        prob = VehicleRoutingProblem(G, load_capacity=robot_load_capacity)
-        prob.num_stops = number_of_stops_per_trip
+        set_node_attributes(G, values=self.demand_each_house, name="demand")
+        G = relabel_nodes(G, {0: "Source", len(self.building_positions): "Sink"})
+        prob = VehicleRoutingProblem(G, load_capacity=self.load_capacity)
+        prob.num_stops = self.stops_per_trip
         prob.solve()
 
-        best_routes = prob.best_routes
-        sorted_house_routes_list = []
-        while best_routes:
-            _, route = best_routes.popitem()
-            sorted_house_routes_list.append(route)
-        sorted_house_routes_list.reverse()
+        routes = prob.best_routes
+        sorted_routes = []
+        while routes:
+            _, route = routes.popitem()
+            sorted_routes.append(route)
+        sorted_routes.reverse()
 
-        for route in sorted_house_routes_list:
+        for route in sorted_routes:
             for i in range(len(route)):
                 if route[i] == "Source":
                     route[i] = -100
                 elif route[i] == "Sink":
                     route[i] = -200
 
-        converted_routes = []
-        for route in sorted_house_routes_list:
+        for route in sorted_routes:
             coords = []
             for node in route:
                 if node not in (-100, -200):
-                    x, y = building_positions[node]
+                    x, y = self.building_positions[node]
                     coords.extend([x, y])
-            converted_routes.append(coords)
+            self.route_queue.append(coords)
 
-        if not converted_routes:
-            continue
-
-        order_ready = SignalOrderReadyRequest()
-        order_ready.botID = bot_id
-        order_ready.deliveryLocations = converted_routes[0]
-
+    def dispatch_loop(self):
         while not rospy.is_shutdown():
             try:
-                ready_response = signal_ready(order_ready)
-                if ready_response.success:
-                    rospy.loginfo("dispatcher: robot %d notified successfully", bot_id)
-                    break
-                else:
-                    rospy.loginfo("dispatcher: waiting for previous robot to pick up order")
+                queue_response = self.listen_queue()
+                bot_id = queue_response.nextInQueue
+                if bot_id < 0:
+                    rospy.loginfo("dispatcher: No robots in queue.")
+                    rospy.sleep(2)
+                    continue
             except rospy.ServiceException:
-                rospy.logerr("dispatcher: failed to call /signal_order_ready")
-            rospy.sleep(2)
+                rospy.logerr("dispatcher: failed to listen to queue")
+                self.rate.sleep()
+                continue
 
-        try:
-            listen_taken()
-            rospy.loginfo("dispatcher: robot %d has taken the order", bot_id)
-        except rospy.ServiceException:
-            rospy.logwarn("dispatcher: failed to confirm order taken")
+            rospy.loginfo(f"dispatcher: Robot {bot_id} is next in queue")
+            if not self.route_queue:
+                self.generate_routes()
+            if not self.route_queue:
+                continue
 
-        rate.sleep()
+            delivery = self.route_queue.pop(0)
+            order_ready = SignalOrderReadyRequest()
+            order_ready.botID = bot_id
+            order_ready.deliveryLocations = delivery
+
+            while not rospy.is_shutdown():
+                try:
+                    response = self.signal_ready(order_ready)
+                    if response.success:
+                        rospy.loginfo(f"dispatcher: robot {bot_id} notified successfully")
+                        break
+                    else:
+                        rospy.loginfo("dispatcher: waiting for previous robot to pick up order")
+                except rospy.ServiceException:
+                    rospy.logerr("dispatcher: failed to signal order readiness")
+                rospy.sleep(2)
+
+            try:
+                self.listen_taken()
+                rospy.loginfo(f"dispatcher: robot {bot_id} has taken the order")
+            except rospy.ServiceException:
+                rospy.logwarn("dispatcher: failed to confirm order taken")
+
+            self.rate.sleep()
 
 if __name__ == "__main__":
     try:
-        dispatcher()
+        Dispatcher().dispatch_loop()
     except rospy.ROSInterruptException:
         pass
